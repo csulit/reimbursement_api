04b68937123b758dc775de4597bd908f6345fdb9
csulit <chrisgelosulit@gmail.com>
2022-08-15 00:38:43 +0800

    Added constant in reimbursements

--- apps/queues/src/queues.controller.ts
+++ apps/queues/src/queues.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { RabbitMqService } from '@app/common';
+import { Controller } from '@nestjs/common';
+import { Ctx, EventPattern, Payload, RmqContext } from '@nestjs/microservices';
+import { CalculateTotal } from './interface/calculate-total.interface';
 import { ReimbursementsQueuesService } from './services/reimbursements.queues.service';
 
 @Controller()
 export class QueuesController {
-  constructor(private readonly queuesService: ReimbursementsQueuesService) {}
+  constructor(
+    private readonly reimbursementsQueuesService: ReimbursementsQueuesService,
+    private readonly rabbitMqService: RabbitMqService,
+  ) {}
 
-  @Get()
-  getHello(): string {
-    return this.queuesService.getHello();
+  @EventPattern('calculate_total')
+  calculateTotal(@Payload() data: CalculateTotal, @Ctx() context: RmqContext) {
+    this.reimbursementsQueuesService.calculateTotal(data);
+    this.rabbitMqService.ack(context);
   }
 }
--- apps/queues/src/services/reimbursements.queues.service.ts
+++ apps/queues/src/services/reimbursements.queues.service.ts
@@ -1,12 +1,54 @@
-import { Injectable } from '@nestjs/common';
+import { PrismaService } from '@app/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { useTryAsync } from 'no-try';
+import { CalculateTotal } from '../interface/calculate-total.interface';
 
 @Injectable()
 export class ReimbursementsQueuesService {
-  getHello(): string {
-    return 'Hello World!';
-  }
+  private readonly logger = new Logger(ReimbursementsQueuesService.name);
+  constructor(private readonly prisma: PrismaService) {}
 
-  logger() {
-    return true;
+  async calculateTotal(data: CalculateTotal) {
+    if (!data?.reimbursement_id) {
+      return false;
+    }
+
+    const { reimbursement_id } = data;
+
+    const [error, record] = await useTryAsync(() =>
+      this.prisma.reimbursement.findUnique({
+        where: { id: reimbursement_id },
+        select: { id: true },
+        rejectOnNotFound: true,
+      }),
+    );
+
+    if (record?.id) {
+      const { id } = record;
+
+      const { _sum } = await this.prisma.particular.aggregate({
+        where: { reimbursement_id: id },
+        _sum: { total: true },
+      });
+
+      await this.prisma.reimbursement.update({
+        where: { id },
+        data: {
+          amount_to_be_reimbursed: _sum.total ?? 0,
+          total_expense: _sum.total ?? 0,
+        },
+        select: {
+          id: true,
+          amount_to_be_reimbursed: true,
+          total_expense: true,
+        },
+      });
+    }
+
+    if (error) {
+      this.logger.error(
+        error?.message || 'Something went wrong in total calculation',
+      );
+    }
   }
 }
